e1eec921e6c81e1cb356a40a7a79afcab3583b46
thetalight <thetalight123@163.com>
2024-12-02 17:04:30 +0800

    add

--- go.mod
+++ go.mod
@@ -1,26 +1,24 @@
 module gnarkcircuit
 
-go 1.22.2
+go 1.23.2
 
 require (
-	github.com/PolyhedraZK/ExpanderCompilerCollection v0.0.2
+	github.com/PolyhedraZK/ExpanderCompilerCollection v0.0.5
 	github.com/consensys/gnark v0.10.0
 	github.com/consensys/gnark-crypto v0.13.0
 )
 
 require (
-	github.com/bits-and-blooms/bitset v1.8.0 // indirect
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/fxamacker/cbor/v2 v2.5.0 // indirect
-	github.com/google/pprof v0.0.0-20230817174616-7a8ec2ada47b // indirect
-	github.com/ingonyama-zk/icicle v0.0.0-20230928131117-97f0079e5c71 // indirect
-	github.com/ingonyama-zk/iciclegnark v0.1.0 // indirect
+	github.com/google/pprof v0.0.0-20240727154555-813a5fbdbec8 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
-	github.com/rs/zerolog v1.30.0 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 	rsc.io/tmplfunc v0.0.3 // indirect
 )
--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"math/big"
 	"os"
+	"time"
+	"fmt"
 
 	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
 
-	"github.com/PolyhedraZK/ExpanderCompilerCollection"
+	"github.com/PolyhedraZK/ExpanderCompilerCollection/ecgo"
 	"github.com/PolyhedraZK/ExpanderCompilerCollection/test"
 )
 
 type Circuit struct {
-	A, B   frontend.Variable `gnark:",public"`
+	A, B   frontend.Variable
 	Result frontend.Variable `gnark:",public"`
 }
 
@@ -20,8 +24,11 @@ func (circuit *Circuit) Define(api frontend.API) error {
 	a := circuit.A
 	b := circuit.B
 
-	for i := 2; i <= 100; i++ {
-		next := api.Add(a, b)
+	// next = a^2 + b^2
+	for i := 2; i <= 10000; i++ {
+		aPower := api.Mul(a,a);
+		bPower := api.Mul(b,b);
+		next := api.Add(aPower, bPower)
 		a, b = b, next
 	}
 	api.AssertIsEqual(circuit.Result, b)
@@ -29,14 +36,40 @@ func (circuit *Circuit) Define(api frontend.API) error {
 }
 
 func main() {
-	circuit, err := ExpanderCompilerCollection.Compile(ecc.BN254.ScalarField(), &Circuit{})
+	proveGnark()
+//   expander()
+}
+
+func proveGnark() {
+	result, _ := new(big.Int).SetString("20158110143150017413199636048408137057957725491036497926379928306369476614404", 10)
+	assignment := &Circuit{A: 0, B: 1, Result: result}
+
+	r1cs, _ := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &Circuit{})
+
+	println("Nb Constraints: ", r1cs.GetNbConstraints())
+
+	witness, _ := frontend.NewWitness(assignment, ecc.BN254.ScalarField())
+	publicWitness, _ := witness.Public()
+	pk, vk, _ := groth16.Setup(r1cs)
+	start := time.Now()
+	proof, _ := groth16.Prove(r1cs, pk, witness)
+	end := time.Now()
+	fmt.Printf("time: %v\n",  end.Sub(start))
+	err := groth16.Verify(proof, vk, publicWitness)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func expander() {
+	circuit, err := ecgo.Compile(ecc.BN254.ScalarField(), &Circuit{})
 	if err != nil {
 		panic(err)
 	}
 
 	c := circuit.GetLayeredCircuit()
 
-	result, _ := new(big.Int).SetString("354224848179261915075", 10)
+	result, _ := new(big.Int).SetString("20158110143150017413199636048408137057957725491036497926379928306369476614404", 10)
 	assignment := &Circuit{A: 0, B: 1, Result: result}
 
 	inputSolver := circuit.GetInputSolver()
